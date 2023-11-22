35b6e4a596695edda989d898203433bf88d646aa
drflamemontgomery <drflamemontgomery@gmail.com>
2023-11-22 14:37:39 +1300

    Added A Test Drawer

--- src/HomeView.hx
+++ src/HomeView.hx
@@ -3,17 +3,26 @@ package;
 import feathers.controls.Panel;
 import feathers.controls.LayoutGroup;
 import feathers.controls.Label;
+import feathers.controls.Button;
+import feathers.controls.Drawer;
 import feathers.layout.AnchorLayout;
+import feathers.layout.AnchorLayoutData;
+import feathers.events.TriggerEvent;
 
 using ComponentBuilder;
 
 // The Startup View for the App
 class HomeView extends Panel {
 
+  private var _container : Drawer;
+
   public function new() {
     super();
     autoSizeMode = STAGE;
-
+    
+    _container = new Drawer();
+    _container.drawer = new LayoutGroup();
+    addChild(_container);
     header = createHomeHeader(); 
   }
 
@@ -22,7 +31,15 @@ class HomeView extends Panel {
       variant : LayoutGroup.VARIANT_TOOL_BAR,
       layout : new AnchorLayout()
     }).withChildren([
-      new Label("Home")
+      new Button("File", ).apply(addEventListener(
+          TriggerEvent.TRIGGER, (event) -> {
+            _container.opened = true;
+          })
+        ),
+      new Label("Home").withFields({
+        layoutData: AnchorLayoutData.center()
+      })
     ]);
   }
+
 }
